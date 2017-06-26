25b06e55d6fb7370dcaf3a3e60536bd1138ed99f
Jonas Lehmann <jonas.lehmann@adfinis-sygroup.ch>
2017-06-26 10:06:29 +0200

    edit language selection with a form, hidden input field and js

--- oss_challenge/osschallenge/static/js/menu.js
+++ oss_challenge/osschallenge/static/js/menu.js
@@ -1,26 +1,8 @@
-function csrfSafeMethod(method) {
-    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-}
-
 $( window ).on("load", function() {
     $(".language-selection").each(function() {
         $(this).on("click", function() {
-            var csrftoken = Cookies.get('csrftoken');
-            $.ajaxSetup({
-                beforeSend: function(xhr, settings) {
-                    if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-                        xhr.setRequestHeader("X-CSRFToken", csrftoken);
-                    }
-                }
-            });
-            $.ajax({
-                method: "POST",
-                url: "/i18n/setlang/",
-                context: $(document),
-                data: { language: $(this).attr('id')}
-            }).done(function(msg) {
-                location.reload();
-            });
+            $('#language-input').attr('value', $(this).attr('id'));
+            $('#language-form').submit();
         });
     });
 });
