0e8b0c4d8384fc5e9ed57b57f90a0a4c6046c76b
Yelin Zhang <yelin.zhang@adfinis-sygroup.ch>
2018-01-12 12:31:35 +0100

    added headless option

--- osschallenge/tests/selenium_test_options.py
+++ osschallenge/tests/selenium_test_options.py
@@ -10,6 +10,5 @@ class SeleniumTests(StaticLiveServerTestCase):
         options = webdriver.ChromeOptions()
         options.add_argument('headless')
         options.add_argument('window-size=1200x600')
-        options.add_argument('no-sandbox')
         self.driver = webdriver.Chrome(chrome_options=options)
         self.driver.implicitly_wait(10)
